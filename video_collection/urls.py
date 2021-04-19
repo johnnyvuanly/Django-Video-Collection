c74b1cf7eb61f3c80406614b2b99f82d51761a91
John L <johnny@example.com>
2021-04-18 21:55:18 -0500

    bug still with adding data

--- video_collection/urls.py
+++ video_collection/urls.py
@@ -1,3 +1,9 @@
 # This will be the urls for our app. Each page needs a URL, View, Template
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    # Call the function in called home in views.py
+    path('', views.home, name='home'),
+    path('add', views.add, name='add_video')
+]
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import VideoForm
 
 # Create your views here.
+def home(request):
+    app_name = 'Coding Videos'
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+def add(request):
+
+    if request.method == 'POST':
+        # Equal to video form the class created from the data that was sent to the server 
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.info(request, 'New video saved!')
+            # TODO show success message or redirect to list of videos
+        else:
+            messages.warning(request, 'Please check the data entered')
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
